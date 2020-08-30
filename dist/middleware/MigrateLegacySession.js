22b49f51ca00731f25795785ae24a4e431eb5bae
buizel <27046219+Pokemonjpups@users.noreply.github.com>
2020-08-30 12:29:11 -0400

    [fix] more babel/staff page issues

--- dist/middleware/MigrateLegacySession.js
+++ dist/middleware/MigrateLegacySession.js
@@ -2,17 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MigrateRBXSession = () => {
     return (req, res, next) => {
-        let cookie = req.cookies['rbxsession'];
-        if (cookie) {
-            res.cookie('blockshub-session', cookie, {
-                secure: true,
-                maxAge: (86400 * 30 * 12) * 1000,
-                sameSite: 'lax',
-                domain: '.blockshub.net',
-            });
-            res.clearCookie('rbxsession');
-            res.redirect('/');
-            return;
+        if (typeof req.cookies === 'object' && req.cookies !== null) {
+            let cookie = req.cookies['rbxsession'];
+            if (typeof cookie === 'string') {
+                res.cookie('blockshub-session', cookie, {
+                    secure: true,
+                    maxAge: (86400 * 30 * 12) * 1000,
+                    sameSite: 'lax',
+                    domain: '.blockshub.net',
+                });
+                res.clearCookie('rbxsession');
+                res.redirect('/');
+                return;
+            }
         }
         next();
     };
--- src/middleware/MigrateLegacySession.ts
+++ src/middleware/MigrateLegacySession.ts
@@ -10,17 +10,19 @@ import config from '../helpers/config';
 
 export const MigrateRBXSession = () => {
     return (req: Request, res: Response, next: NextFunction) => {
-        let cookie = req.cookies['rbxsession'];
-        if (cookie) {
-            res.cookie('blockshub-session', cookie, {
-                secure: true,
-                maxAge: (86400 * 30 * 12) * 1000,
-                sameSite: 'lax',
-                domain: '.blockshub.net',
-            })
-            res.clearCookie('rbxsession');
-            res.redirect('/');
-            return
+        if (typeof req.cookies === 'object' && req.cookies !== null) {
+            let cookie = req.cookies['rbxsession'];
+            if (typeof cookie === 'string') {
+                res.cookie('blockshub-session', cookie, {
+                    secure: true,
+                    maxAge: (86400 * 30 * 12) * 1000,
+                    sameSite: 'lax',
+                    domain: '.blockshub.net',
+                })
+                res.clearCookie('rbxsession');
+                res.redirect('/');
+                return
+            }
         }
         next();
     }
