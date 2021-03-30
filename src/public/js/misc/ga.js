547a9e82bddaf1dfe79036f935264872b6fae56b
10allday <45305936+10allday@users.noreply.github.com>
2021-03-30 15:40:04 -0600

    Update ga.js

--- src/public/js/misc/ga.js
+++ src/public/js/misc/ga.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
-gtag('config', 'UA-161288601-1');
+gtag('config', 'GTM-WRD4X3R');
