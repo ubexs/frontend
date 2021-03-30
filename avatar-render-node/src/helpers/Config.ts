5d4b1e0d7a63808ae6987538960bd207e6e22ce7
10allday <45305936+10allday@users.noreply.github.com>
2021-03-30 15:25:23 -0600

    Delete avatar-render-node directory

--- avatar-render-node/src/helpers/Config.ts
+++
@@ -1,7 +0,0 @@
-interface IWebsiteConfiguration {
-    authorization: string;
-    port?: number;
-}
-
-const conf: Readonly<IWebsiteConfiguration> = JSON.parse(require('fs').readFileSync(require('path').join(__dirname, '../../config.json')).toString())
-export default conf;
--- avatar-render-node/src/helpers/Exceptions.ts
+++
@@ -1,7 +0,0 @@
-import * as HTTPExceptions from 'ts-httpexceptions';
-
-export default class StdExceptions {
-    public NotFound = HTTPExceptions.NotFound;
-    public BadRequest = HTTPExceptions.BadRequest;
-    public InternalServerError = HTTPExceptions.InternalServerError;
-}
