c522b7154234d4be56aefb4d1fc48ee1c0564288
10allday <45305936+10allday@users.noreply.github.com>
2021-12-11 07:30:36 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { $log } from "@tsed/common";
 import { PlatformExpress } from "@tsed/platform-express";
-import { Server } from "Server";
+import { Server } from "./server.ts";
 
 async function bootstrap() {
     try {
